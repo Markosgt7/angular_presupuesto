8314a4dbf6c0827ee24829bc3bbf09c206a6fd66
Markosgt7 <markos.guate7@gmail.com>
2023-08-08 23:32:13 -0600

    validacion de campos en ingreso de gasto

--- src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
+++ src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ingresar-gasto',
   templateUrl: './ingresar-gasto.component.html',
-  styleUrls: ['./ingresar-gasto.component.css']
+  styleUrls: ['./ingresar-gasto.component.css'],
 })
 export class IngresarGastoComponent {
-
+  nombreGasto: string;
+  cantidad: number;
+  formularioIncorrecto: boolean;
+  textIncorrecto: string;
+  constructor() {
+    this.nombreGasto = '';
+    this.cantidad = 0;
+    this.formularioIncorrecto = false;
+    this.textIncorrecto = 'Descripción del  gasto o cantidad Incorrecta';
+  }
+  agregarGasto() {
+    if (this.nombreGasto == '' || this.cantidad <= 0) {
+      this.formularioIncorrecto = true;
+    } else {
+      this.formularioIncorrecto = false;
+      this.nombreGasto = '';
+      this.cantidad = 0;
+    }
+  }
 }
